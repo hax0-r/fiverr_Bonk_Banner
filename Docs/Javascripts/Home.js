ae2f57262b3dae53960f8b4e976ba85cba11289a
Muhammad <talha18513@gmail.com>
2025-01-19 16:44:51 +0500

    more pages add

---
+++ Docs/Javascripts/Home.js
@@ -0,0 +1,77 @@
+const cards = [
+    {
+        image: "./Docs/Assets/mobile1.png",
+        title: "CASHBACK",
+        description: "Customer cashback as an incentive for future purchases."
+    },
+    {
+        image: "./Docs/Assets/mobile5.png",
+        title: "STAMPS",
+        description: "Buy 9, get the 10th free."
+    },
+    {
+        image: "./Docs/Assets/mobile6.png",
+        title: "REWARDS",
+        description: "Points system for your loyal customers."
+    },
+    {
+        image: "./Docs/Assets/mobile7.png",
+        title: "MEMBERSHIP",
+        description: "Create your VIP loyalty club for exclusive members."
+    },
+    {
+        image: "./Docs/Assets/mobile8.png",
+        title: "DISCOUNTS",
+        description: "Manage wholesale sales with discounts."
+    },
+    {
+        image: "./Docs/Assets/mobile9.png",
+        title: "COUPON",
+        description: "Perfect for ads. It transforms into a new one after being used."
+    },
+    {
+        image: "./Docs/Assets/mobile10.png",
+        title: "PREPAID",
+        description: "Advance payments with prepaid stamp cards."
+    },
+    {
+        image: "./Docs/Assets/mobile11.png",
+        title: "GIFTCARDS",
+        description: "Advance payments with prepaid gifts."
+    },
+];
+const container = document.querySelector(".mobileCards");
+
+cards.forEach(card => {
+    const cardHTML = `
+    <div class="w-full bg-[#e6e6e6] md:rounded-[2.5rem] rounded-3xl border-4 border-white overflow-hidden md:p-4 p-2">
+        <img src="${card.image}" class="w-full" alt="${card.title}">
+        <h2 class="font-bold text-center md:text-3xl text-2xl pt-6 md:pt py-3.5">${card.title}</h2>
+        <p class="text-center md:font-semibold font-medium md:text-lg pb-2 md:leading-tight">${card.description}</p>
+    </div>
+`;
+    container.innerHTML += cardHTML;
+});
+
+
+document.addEventListener('alpine:init', () => {
+    Alpine.store('accordion', {
+        tab: 0
+    });
+
+    Alpine.data('accordion', (idx) => ({
+        init() {
+            this.idx = idx;
+        },
+        idx: -1,
+        handleClick() {
+            this.$store.accordion.tab = this.$store.accordion.tab === this.idx ? 0 : this.idx;
+        },
+        handleRotate() {
+            return this.$store.accordion.tab === this.idx ? 'rotate-180 opacity-0' : '';
+        },
+        handleToggle() {
+            return this.$store.accordion.tab === this.idx ? `max-height: ${this.$refs.tab.scrollHeight}px` : '';
+        }
+    }));
+})
--- Docs/Javascripts/global.js
+++ Docs/Javascripts/global.js
@@ -1,18 +1,79 @@
-const lenis = new Lenis();
-function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
-
 const responsiveNavbar = document.getElementById("responsiveNavbar")
-const closeNav = document.getElementById("closeNav")
+const closeNav = document.getElementsByClassName("closeNav")
 const openNav = document.getElementById("openNav")
 
 openNav.addEventListener('click', () => {
     responsiveNavbar.style.width = "100%"
 })
 
-closeNav.addEventListener('click', () => {
-    responsiveNavbar.style.width = "0%"
-})
+Array.from(closeNav).forEach(button => {
+    button.addEventListener('click', () => {
+        responsiveNavbar.style.width = "0%";
+    });
+});
+
+
+// contact Popup
+
+
+const contectPopup = document.getElementById("contectPopup");
+const contectPopupOverlay = document.getElementById("contectPopupOverlay");
+const contectPopupOpen = document.getElementsByClassName("contectPopupOpen");
+
+let popupShowCount = 0;
+const popupIntervals = [32 * 1000, 150 * 1000];
+
+Array.from(contectPopupOpen).forEach((open) => {
+    open.addEventListener('click', () => {
+        contectPopup.style.display = "block"; 
+        setTimeout(() => {
+            contectPopup.style.opacity = "1";
+        }, 10); 
+    });
+});
+
+function showPopup() {
+    if (popupShowCount < popupIntervals.length) {
+        contectPopup.style.display = "block";
+        setTimeout(() => {
+            contectPopup.style.opacity = "1";
+        }, 10);
+    }
+}
+
+function hidePopup() {
+    contectPopup.style.opacity = "0";
+    setTimeout(() => {
+        contectPopup.style.display = "none";
+        popupShowCount++;
+        if (popupShowCount < popupIntervals.length) {
+            setTimeout(showPopup, popupIntervals[popupShowCount]);
+        }
+    }, 500);
+}
+
+contectPopupOverlay.addEventListener('click', hidePopup);
+
+setTimeout(showPopup, popupIntervals[0]);
+
+
+const AffiliateBusiness = document.getElementById("AffiliateBusiness");
+const AffiliateBusinessOverlay = document.getElementById("AffiliateBusinessOverlay");
+const AffiliateBusinessOpen = document.getElementsByClassName("AffiliateBusinessOpen");
+
+AffiliateBusinessOverlay.addEventListener('click', () => {
+    AffiliateBusiness.style.opacity = "0"; 
+    setTimeout(() => {
+        AffiliateBusiness.style.display = "none";
+    }, 500); 
+});
+
+Array.from(AffiliateBusinessOpen).forEach((open) => {
+    open.addEventListener('click', () => {
+        AffiliateBusiness.style.display = "block"; 
+        setTimeout(() => {
+            AffiliateBusiness.style.opacity = "1";
+        }, 10); 
+    });
+});
+
